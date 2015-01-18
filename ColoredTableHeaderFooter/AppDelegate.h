7c98b31bfad72393ff5c97963b1009e3f58bf9a0
Markus Schmid <schmidgpms@t-online.de>
2015-01-18 06:24:43 +0100

    Initial Commit

---
+++ ColoredTableHeaderFooter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ColoredTableHeaderFooter
+//
+//  Created by Markus Schmid on 18.01.15.
+//  Copyright (c) 2015 Markus Schmid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ColoredTableHeaderFooter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ColoredTableHeaderFooter
+//
+//  Created by Markus Schmid on 18.01.15.
+//  Copyright (c) 2015 Markus Schmid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
